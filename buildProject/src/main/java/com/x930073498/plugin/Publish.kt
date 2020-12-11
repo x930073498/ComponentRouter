1fc4adc42f0cd2ef902d8d483c1b616a6633884c
x930073498 <x930073498@gmail.com>
2020-12-12 04:45:10 +0800

    测试项目可用

--- buildProject/src/main/java/com/x930073498/plugin/Publish.kt
+++ buildProject/src/main/java/com/x930073498/plugin/Publish.kt
@@ -5,7 +5,7 @@ import org.gradle.kotlin.dsl.extra
 
 object Publish {
     const val GROUP = "com.x930073498.component"
-    const val VERSION = "0.0.6"
+    const val VERSION = "0.0.7"
     enum class PublishInfo(
         val group: String,
         val artifact: String,
